71f6d9aa114780ada5c350b0da9124eeccb63af0
yiliang114 <1204183885@qq.com>
2023-07-15 19:46:43 +0800

    Update a.js

--- src/a.js
+++ src/a.js
@@ -5,7 +5,7 @@ async function a() {
     const COMMIT_EMAIL =
       "41898282+github-actions[bot]@users.noreply.github.com";
     const COMMIT_NAME = "github-actions[bot]";
-    const TARGET_FILE = "a.js";
+    const TARGET_FILE = "src/a.js";
     const COMMIT_MSG = "Update a.js";
     await commitFile(COMMIT_EMAIL, COMMIT_NAME, TARGET_FILE, COMMIT_MSG);
   } catch (err) {
