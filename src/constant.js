9a53b11422c2651e9fabfd4260517c92e0d53d26
yiliang114 <1204183885@qq.com>
2023-07-15 16:04:46 +0800

    多 users 和 多 repos 支持

--- src/constant.js
+++ src/constant.js
@@ -1,5 +1,3 @@
-const dayjs = require("dayjs");
-
 // 时间格式
 const format = "YYYY-MM-DD HH:mm:ss";
 
--- src/utils/time.js
+++ src/utils/time.js
@@ -1,3 +1,4 @@
+const dayjs = require("dayjs");
 const { format } = require("../constant");
 const formatDate = (date) => dayjs(date).format(format);
 
